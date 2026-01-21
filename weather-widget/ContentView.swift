e3e8742bbc6cab44864ca7402530306532842ccc
BenOlivier <dbeolivier@gmail.com>
2026-01-21 13:59:16 +0000

    Improve cloud texture

--- weather-widget/ContentView.swift
+++ weather-widget/ContentView.swift
@@ -12,10 +12,14 @@ struct ContentView: View {
     
     @State var dampedPitch: Double = 0.0
     @State var dampedRoll: Double = 0.0
-    @State private var parallaxOffset: CGSize = .zero
     
     let motionManager = CMMotionManager()
     let decayHz: Double = 1.5
+    
+    let CloudParallax: Double = 100
+    let RainParallax: Double = 40
+    let FrameParallax: Double = 20
+    let ShadowParallax: Double = 20
     
     var body: some View {
         ZStack {
@@ -25,28 +29,34 @@ struct ContentView: View {
             
             ZStack{
                 ZStack(alignment: .top) {
-                    Color("background")
+                    LinearGradient(
+                        colors: [Color("background-1"), Color("background-2")],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
                     
                     Image("clouds")
                         .resizable()
                         .scaledToFit()
-                        .opacity(0.5)
-                        .padding(.top, 50)
+                        .opacity(0.4)
+                        .scaleEffect(1.6)
+                        .offset(x: CGFloat(dampedRoll * CloudParallax), y: CGFloat(dampedPitch * CloudParallax))
                     
                     RainCanvasLayer(
                         dropsPer10kPixels: 80,
-                        globalFallSpeed: 1.2,
+                        globalFallSpeed: 0.2,
                         baseDropLength: 8,
                         baseDropThickness: 0.75,
                         farOpacity: 0.2,
                         nearOpacity: 0.3,
                         blurRadius: 0
                     )
+                    .scaleEffect(1.2)
+                    .offset(x: CGFloat(dampedRoll * RainParallax), y: CGFloat(dampedPitch * RainParallax))
                 }
                 .frame(width: 300, height: 400)
-                .scaleEffect(1.6)
-                .offset(x: CGFloat(dampedRoll * 90), y: CGFloat(dampedPitch * 90))
                 .clipShape(RoundedRectangle(cornerRadius: 24, style: .continuous))
+//                .offset(x: CGFloat(dampedRoll * FrameParallax), y: CGFloat(dampedPitch * FrameParallax))
                 .animation(.linear(duration: 0.1), value: dampedPitch)
                 .animation(.linear(duration: 0.1), value: dampedRoll)
                 
