f72bfd0d411dd8e05ced9c9936e7e5c0b91f5b21
BenOlivier <dbeolivier@gmail.com>
2026-01-21 08:58:21 +0000

    Initial Commit

---
+++ weather-widget/weather_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_widgetApp.swift
+//  weather-widget
+//
+//  Created by Ben Olivier on 21/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct weather_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
